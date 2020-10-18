e2fddb2bd72e5283050d0318cff84b369e2605af
Van <vango85@gmail.com>
2020-10-18 22:34:27 +0200

    [xx] Starting point

--- src/CounterApp.js
+++ src/CounterApp.js
@@ -1,29 +1,7 @@
-import React, { useState } from "react";
-import CounterInput from "./CounterInput";
-import CounterSummary from "./CounterSummary";
+import React from "react";
 
 const CounterApp = () => {
-  const [clickCount, setClickCount] = useState(0);
-
-  const handleAddToCount = (numToAdd) => {
-    setClickCount(clickCount + numToAdd);
-  };
-
-  console.log(`New total:  ${clickCount}`);
-
-  return (
-    <section className="counter-app">
-      <h1>Counter App</h1>
-      {clickCount > 20 ? (
-        <CounterSummary clickCount={clickCount}></CounterSummary>
-      ) : (
-        <CounterInput
-          clickCount={clickCount}
-          handleAddToCount={handleAddToCount}
-        />
-      )}
-    </section>
-  );
+  return <></>;
 };
 
 export default CounterApp;
--- src/CounterSummary.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const CounterSummary = ({ clickCount }) => {
-  return <p class="counter-summary">Summary: {clickCount}</p>;
-};
-
-export default CounterSummary;
