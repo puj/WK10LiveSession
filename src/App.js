32ae9bcdeb0368900fe0a940fa4e6f02a191d7fc
Van <vango85@gmail.com>
2020-10-18 21:42:05 +0200

    [xx] Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Survey from "./Survey.js";
+
+export const App = () => {
+  return (
+    <section className="wrapper-section">
+      <Survey />
+    </section>
+  );
+};
---
+++ src/Survey.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Survey = () => {
+  return (
+    <form>
+      <input type="submit"></input>
+    </form>
+  );
+};
+
+export default Survey;
